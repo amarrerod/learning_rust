8b22fad1031a24461fb97b8fb0dbb218d1735fb8
Alejandro Marrero <alemarrerodiaz@gmail.com>
2021-04-28 15:57:54 +0100

    feat: Includes Hello World in Rust

---
+++ examples/hello_world.rs
@@ -0,0 +1,14 @@
+
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}, {}!", "Hello", "world");
+    println!("{0}, {1}!", "Hello", "world");
+    println!("{greeting}, {name}!", greeting = "Hello", name = "world");
+    println!("{:?}", [1, 2, 3]);
+    println!("{:#?}", [1, 2, 3]);
+
+    let x = format!("{}, {}!", "Hello", "world"); // Cadena formateada
+    println!("{}", x);
+    
+}
